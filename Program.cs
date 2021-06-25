5c259783bb2d06a8a54cc2449fdd5451b9057217
Dan Kronstal <dankronstal@gmail.com>
2021-06-24 22:41:06 -0600

    update with web ui and auto save/sync

--- Program.cs
+++ Program.cs
@@ -16,19 +16,27 @@ namespace HoloLensModelCoAuthorSK
 
         public class Runner
         {
+            static Uri uri = new Uri("https://danslab.blob.core.windows.net/$web/HoloLensModelCoAuthorSKweb/model2.csv?sp=racwd&st=2021-06-24T21:05:29Z&se=2022-06-25T05:05:29Z&spr=https&sv=2020-02-10&sr=b&sig=rVUT7VaQoe%2FKDte3hHgGEBrOKpKgnBBfgqtZ69%2FnTIw%3D");
+            static int autoupdateinterval = 5; //in seconds
+            static Pose logPose = new Pose(new Vec3(0.4f, 0.2f, -0.4f), Quat.LookDir(-1, 0, 1));
+            static List<string> logList = new List<string>();
+
             List<Vec3> meshPoints;
             List<Pose> meshPoses;
             List<Model> meshModels;
 
+            bool autoupdateflag = false;
+
             public void GetRunning()
             {
+                DateTime dtNow, dtLast = DateTime.UtcNow;
+
                 meshPoints = new List<Vec3>();
                 meshPoses = new List<Pose>();
                 meshModels = new List<Model>();
 
                 GrabMeshData();
 
-                // Initialize StereoKit
                 SKSettings settings = new SKSettings
                 {
                     appName = "HoloLensModelCoAuthorSK",
@@ -39,23 +47,22 @@ namespace HoloLensModelCoAuthorSK
 
                 Pose windowPose = new Pose(new Vec3(0.3f, 0, -0.3f), Quat.LookDir(-1, 0, 1));
 
-                // Update mesh
                 foreach (var v in meshPoints)
                 {
                     meshPoses.Add(new Pose(v, Quat.Identity));
                     meshModels.Add(Model.FromMesh(Mesh.GenerateSphere(0.1f, 10), Default.MaterialUI));
                 }
 
-                Hand hand = Input.Hand(Handed.Right);
-
-                // Core application loop
                 while (SK.Step(() =>
-                {                    
+                {
+                    dtNow = DateTime.UtcNow;
+                    Hand hand = Input.Hand(Handed.Right);
+                    autoupdateflag = ((TimeSpan)(dtNow - dtLast)).TotalSeconds >= autoupdateinterval && !hand.IsPinched;
                     UI.WindowBegin("Settings", ref windowPose, new Vec2(20, 0) * U.cm);
-                    if (UI.Button("Update"))
+                    if (UI.Button("Update") || autoupdateflag)
                     {
+                        dtLast = DateTime.UtcNow;
                         GrabMeshData();
-                        // Update mesh
                         foreach (var v in meshPoints)
                         {
                             meshPoses.Add(new Pose(v, Quat.Identity));
@@ -63,59 +70,79 @@ namespace HoloLensModelCoAuthorSK
                         }
                     }
                     if (UI.Button("Save"))
-                    {
                         PushMeshData();
-                    }
+
                     UI.WindowEnd();
+
+                    LogWindow();
 
                     if (meshPoints.Count > 0)
                         for (int i = 0; i < meshPoses.Count; i++)
                         {
                             Pose p = meshPoses[i];
                             Model m = meshModels[i];
-                            UI.Handle("Cube" + i, ref p, m.Bounds);
-                            //TODO: add event for move completed: if(UI.Handle(...)) if(hand.IsJustUnpinched)  <do some stuff, like update meshPoint[i] with new location and autosave>
+                            if (UI.Handle("Cube" + i, ref p, m.Bounds))
+                                if (hand.IsJustUnpinched)
+                                    UpdatePointsFromMesh(i, p.position);                                
                             m.Draw(p.ToMatrix());
                             meshPoses[i] = p;
                         }
-
-                    if (hand.IsJustUnpinched)
-                        PushMeshData();
-                })) ;
+                }));
                 SK.Shutdown();
             }
+
+            private void LogWindow()
+            {
+                UI.WindowBegin("Log", ref logPose, new Vec2(40, 0) * U.cm);
+                for (int i = 0; i < logList.Count; i++)
+                    UI.Label(logList[i], false);
+                UI.WindowEnd();
+            }
+
+            private void OnLog(LogLevel level, string text)
+            {
+                if (logList.Count > 10)
+                    logList.RemoveAt(logList.Count - 1);
+                logList.Insert(0, text.Length < 100 ? text : text.Substring(0, 100) + "...");
+            }
+
+            private void UpdatePointsFromMesh(int i, Vec3 v)
+            {
+                OnLog(LogLevel.Info, "updating [" + i + "] from (" + meshPoints[i] + ") to (" + v.ToString() + "]");
+                meshPoints[i] = v;
+                PushMeshData();
+            }
+
             public void GrabMeshData()
             {
                 meshPoints = new List<Vec3>();
                 meshPoses = new List<Pose>();
                 meshModels = new List<Model>();
 
-                WebRequest request = WebRequest.Create("https://danslab.blob.core.windows.net/$web/model.csv");
+                WebRequest request = WebRequest.Create(uri);
                 WebResponse response = request.GetResponse();
-                Console.WriteLine("");
                 using (var reader = new StreamReader(response.GetResponseStream()))
                 {
                     while (!reader.EndOfStream)
                     {
                         var line = reader.ReadLine();
                         var values = line.Split(',');
-
                         meshPoints.Add(new Vec3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2])));
                     }
                 }
-
-                UpdateMesh(meshPoints);
             }
 
             public void PushMeshData()
             {
-                Console.WriteLine("PUSHING!!!!!");
                 //TODO: implement Storage Account saving, per docs here: https://docs.microsoft.com/en-us/dotnet/api/azure.storage.blobs.blobclient.upload?view=azure-dotnet#Azure_Storage_Blobs_BlobClient_Upload_System_String_Azure_Storage_Blobs_Models_BlobUploadOptions_System_Threading_CancellationToken_
-            }
-
-            public void UpdateMesh(List<Vec3> points)
-            {
-                //TODO: Move update code in here
+                //      until then, the below is a quick/hacky way to get it up there
+                string csvOut = "";
+                for (int i = 0; i < meshPoints.Count; i++)
+                    csvOut += meshPoints[i].x + "," + meshPoints[i].y + "," + meshPoints[i].z + (i == meshPoints.Count - 1 ? "" : "\r\n");
+                OnLog(LogLevel.Info, csvOut);
+                WebClient wc = new WebClient();
+                wc.Headers.Add("x-ms-blob-type", "BlockBlob");
+                wc.UploadData(uri, "PUT", System.Text.Encoding.ASCII.GetBytes(csvOut));
             }
         }
     }
---
+++ readme.md
@@ -0,0 +1,16 @@
+﻿# HoloLensModelCoAuthorSK
+This project is a PoC which attempts to showcase the ability to "coauthor" a model using a combination of a HoloLens 2 app developed with [StereoKit](https://www.stereokit.net) and a web UI. 
+# Required Environment
+Should be pretty quick and easy to pull down the repo and build with Visual Studio 2019. No extras required, as long as you can pull down the NuGet packages for:
+- Microsoft.NETCore.UniversalWindowsPlatform (v6.2.9)
+- StereoKit (v0.3.1)
+
+A couple of hardcoded spots will need updating in the code, including:
+- values for Azure Blob Storage account/container/folder (or whatever other location you'd care to use to store the files. Check the Program.cs and Web/index.html files for the Uri property and update that. 
+- autoupdateinterval property to change how often changes sync. Default is 5 seconds.
+- model filename. Default is model.csv.
+# Running Output
+![Video of running code](Assets/demo.gif)
+# Notes
+Solution needs restructuring, and a few TODOs resolved.
+Next step is to add edge and face elements, but for this PoC likely will leave it as single/solid model. More complex implementation will require a total overhaul.
